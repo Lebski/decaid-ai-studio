b46b60d8ff03bcd9f5cfe6b090cf3aeafb3a798a
Daniel Kunigk <daniel@levire.com>
2024-09-03 14:22:31 +0200

    Reuse common elements for each section

--- src/components/Home/ContentCreationSection.tsx
+++ src/components/Home/ContentCreationSection.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import dotsIcon from 'assets/images/dots-vertical.svg';
 import editIcon from 'assets/images/edit-04.svg';
 import searchIcon from 'assets/images/search-refraction.svg';
 import ContentCreationCard from './ContentCreationCard';
+import SectionWrapper from 'components/SectionWrapper';
 
 const ContentCreationSection: React.FC = () => {
   const handleCreatePost = () => {
@@ -16,35 +16,24 @@ const ContentCreationSection: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col w-full max-md:max-w-full">
-      <div className="flex flex-wrap gap-4 w-full max-md:max-w-full">
-        <h2 className="flex-1 shrink text-lg font-semibold leading-7 text-gray-900 min-w-[240px] max-md:max-w-full">
-          Start creating content
-        </h2>
-        <div className="flex flex-col self-start w-5">
-          <img src={dotsIcon} alt="" className="object-contain w-5 aspect-square" />
+    <SectionWrapper title="Start creating content">
+      <div className="flex flex-col flex-1 shrink w-full basis-0 min-w-[240px] max-md:max-w-full">
+        <div className="flex flex-wrap gap-6 items-start w-full max-md:max-w-full">
+          <ContentCreationCard
+            icon={editIcon}
+            title="Create a new post"
+            description="Dive into the editor and start creating"
+            action={handleCreatePost}
+          />
+          <ContentCreationCard
+            icon={searchIcon}
+            title="Find relevant Topics"
+            description="Search the news to find topics to talk about"
+            action={handleFindTopics}
+          />
         </div>
       </div>
-      <div className="flex mt-5 w-full bg-gray-200 min-h-[1px] max-md:max-w-full" />
-      <div className="flex gap-8 items-start mt-6 w-full max-md:max-w-full">
-        <div className="flex flex-col flex-1 shrink w-full basis-0 min-w-[240px] max-md:max-w-full">
-          <div className="flex flex-wrap gap-6 items-start w-full max-md:max-w-full">
-            <ContentCreationCard
-              icon={editIcon}
-              title="Create a new post"
-              description="Dive into the editor and start creating"
-              action={handleCreatePost}
-            />
-            <ContentCreationCard
-              icon={searchIcon}
-              title="Find relevant Topics"
-              description="Search the news to find topics to talk about"
-              action={handleFindTopics}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    </SectionWrapper>
   );
 };
 
---
+++ src/components/SectionWrapper.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import dotsIcon from 'assets/images/dots-vertical.svg';
+
+interface SectionWrapperProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const SectionWrapper: React.FC<SectionWrapperProps> = ({ title, children }) => {
+  return (
+    <div className="flex flex-col mt-8 w-full max-md:max-w-full">
+      <div className="flex flex-col w-full max-md:max-w-full">
+        <div className="flex flex-wrap gap-4 w-full max-md:max-w-full">
+          <h2 className="flex-1 shrink text-lg font-semibold leading-7 text-gray-900 whitespace-nowrap min-w-[240px] max-md:max-w-full">
+            {title}
+          </h2>
+          <div className="flex flex-col self-start w-5">
+            <img src={dotsIcon} alt="" className="object-contain w-5 aspect-square" />
+          </div>
+        </div>
+        <div className="flex mt-5 w-full bg-gray-200 min-h-[1px] max-md:max-w-full" />
+      </div>
+      <div className="flex flex-wrap gap-12 items-start mt-6 w-full max-md:max-w-full">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper;
