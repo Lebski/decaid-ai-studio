d7d8022bca6a2205f56c073dd299c4cdc30c5ff4
Daniel Kunigk <daniel@levire.com>
2024-09-05 10:15:24 +0200

    Move integration data to research page

--- src/components/Research/SourcesSection.tsx
+++ src/components/Research/SourcesSection.tsx
@@ -5,15 +5,6 @@ import Button from "components/Forms/Button";
 import IntegrationList from "./IntegrationList";
 
 import plusIcon from "assets/images/plus.svg";
-import apIcon from 'assets/images/source-icons/ap.png';
-import zeitIcon from 'assets/images/source-icons/zeit.png';
-import tagesschauIcon from 'assets/images/source-icons/tagesschau.png';
-import guardianIcon from 'assets/images/source-icons/guardian.png';
-import nytimesIcon from 'assets/images/source-icons/nyt.png';
-import hnIcon from 'assets/images/source-icons/hackernews.png';
-import hfIcon from 'assets/images/source-icons/huggingface.png';
-import redditIcon from 'assets/images/source-icons/reddit.png';
-import bensbitesIcon from 'assets/images/source-icons/bens-bites.png';
 
 export interface Integration {
     name: string;
@@ -22,72 +13,6 @@ export interface Integration {
     isActive: boolean;
     category: string;
 }
-
-export const initialIntegrations: Integration[] = [
-    {
-        name: "Hacker News",
-        imageSrc: hnIcon,
-        description: "Streamline software projects, sprints, and bug tracking.",
-        isActive: false,
-        category: "tech"
-    },
-    {
-        name: "HuggingFace",
-        imageSrc: hfIcon,
-        description: "Link pull requests and automate workflows.",
-        isActive: true,
-        category: "tech"
-    },
-    {
-        name: "Reddit",
-        imageSrc: redditIcon,
-        description: "Embed file previews in projects.",
-        isActive: false,
-        category: "social"
-    },
-    {
-        name: "Ben's Bites",
-        imageSrc: bensbitesIcon,
-        description: "AI for Non-Techies — Learn how AI tools can transform your productivity",
-        isActive: false,
-        category: "tech"
-    },
-    {
-        name: "Die Zeit",
-        imageSrc: zeitIcon,
-        description: "Die Zeit is a German national weekly newspaper published in Hamburg in Germany.",
-        isActive: true,
-        category: "news"
-    },
-    {
-        name: "Tagesschau",
-        imageSrc: tagesschauIcon,
-        description: "Die erste Adresse für Nachrichten und Information: An 365 Tagen im Jahr, rund um die Uhr aktualisiert.",
-        isActive: true,
-        category: "news"
-    },
-    {
-        name: "AP",
-        imageSrc: apIcon,
-        description: "The Associated Press is an American not-for-profit news agency headquartered in New York City.",
-        isActive: true,
-        category: "news"
-    },
-    {
-        name: "The Guardian",
-        imageSrc: guardianIcon,
-        description: "The Guardian is a British daily newspaper. It was founded in 1821 as The Manchester Guardian.",
-        isActive: true,
-        category: "news"
-    },
-    {
-        name: "The New York Times",
-        imageSrc: nytimesIcon,
-        description: "The New York Times is an American daily newspaper based in New York City.",
-        isActive: false,
-        category: "news"
-    }
-];
 
 interface SourcesSectionProps {
     integrations: Integration[];
